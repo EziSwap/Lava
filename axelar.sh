377348e9630184f4b5175cde93dea26872c50858
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-04-27 22:48:20 +0300

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-13b3d59e-d8e7-4dba-af86-8a693bba41f3/"
 
 OUTPUT_DIR="logs"
 
-axelar_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+axelar_data=$(curl -A UA -x http://premium.proxywing.com:12321 --proxy-user d9hv1twu6q:rzld6kr3mb -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
